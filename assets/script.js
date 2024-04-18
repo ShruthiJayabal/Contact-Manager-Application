f21725e97cb926a50baa0014a9bdc419f7d98d5f
ShruthiJayabal <shruthijayabal07@gmail.com>
2024-04-18 18:30:32 +0530

    added button click event

--- assets/script.js
+++ assets/script.js
@@ -49,7 +49,7 @@ addContactForm.addEventListener('submit', (e) => {
   contacts.push({ name, number, email, address });
   saveContacts();
   displayContacts();
-  contactForm.style.display = 'flex';
+  contactForm.style.display = 'none';
   addContactForm.reset();
 });
 
